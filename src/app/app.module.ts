23c78bcf552a808b74a6e3e6df8a66a968f76584
minatorak <hotmail22866@gmail.com>
2018-07-14 15:00:37 +0700

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BnkGirlListComponent } from './components/bnk-girl-list/bnk-girl-list.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Route } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BnkGirlComponent } from './components/bnk-girl/bnk-girl.component';
+import { InstagramComponent } from './components/instagram/instagram.component';
 
+
+const routes: Route[] = [
+  {path: '', pathMatch: 'full', redirectTo: '/dashboard'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'instagram/:instagramId', component: InstagramComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     BnkGirlListComponent,
-    BnkGirlComponent
+    BnkGirlComponent,
+    InstagramComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
