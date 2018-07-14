a5fe0b399987fc078c873cc3735852ce548ce641
minatorak <hotmail22866@gmail.com>
2018-07-14 14:14:49 +0700

    add https

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BnkGirlListComponent } from './components/bnk-girl-list/bnk-girl-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -15,7 +16,8 @@ import { BnkGirlComponent } from './components/bnk-girl/bnk-girl.component';
     BnkGirlComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
